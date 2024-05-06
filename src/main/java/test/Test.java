81c7fed32e1f9a645c3d34d3ef91ba6db2190945
Sylvain Colomer <sylvain.colomer@batch.com>
2024-05-06 13:13:15 +0200

    test

--- src/main/java/test/Test.java
+++ src/main/java/test/Test.java
@@ -1,16 +1,21 @@
 package test;
 
-import java.util.ArrayList;
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
 import javax.annotation.ParametersAreNonnullByDefault;
+// import org.jetbrains.annotations.NotNull;
+// import javax.annotation.Nonnull;
+// import javax.annotation.Nullable;
+// import javax.annotation.ParametersAreNonnullByDefault;
 import test.test2.Test12;
 
 @ParametersAreNonnullByDefault
 public class Test {
 
-  public static void test(Test12 test) {}
+    public static void test(Test12 test) {}
 
-  public static void main(String[] args) {
-    //   var lst = new Test12();
-    test(null);
-  }
+    public static void main(String[] args) {
+        //   var lst = new Test12();
+        test(null);
+    }
 }
